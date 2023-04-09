8a348d8b802701cb45dbdc2820523c97d92c4bd0
Alissa Sheikh <89821902+asheikh3378@users.noreply.github.com>
2023-04-09 14:53:38 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+parts = input().split()
+name = parts[0]
+while name != '-1':
+    try:
+        age = int(parts[1]) + 1
+    except ValueError:
+        age = 0
+    print(f'{name} {age}')
+    parts = input().split()
+    name = parts[0]
